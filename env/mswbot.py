cb72480afae835a9e9d5d4cf40a7ba6fd5124fb4
hkgnp <ben@pngs.cc>
2021-04-29 00:00:42 +0800

    Trying api

--- env/mswbot.py
+++ env/mswbot.py
@@ -3,17 +3,26 @@ from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import config
 import json
 from pathlib import Path
+import requests
 
 # 1. Get dictionary
 # 2. Assign each object to a variable
 # 3. Call variable when user triggers keyword
 
+# Retrieving from local json file
 def retrieve():
     path = Path(__file__).parent / "resources.json"
     with path.open() as json_file:
         data = json.load(json_file)
-
     return data
+
+# Retrieving from api
+def retrieve_ics():
+    response = requests.get("https://icschecker.herokuapp.com/api/index")
+    print(response.content())
+    print(response.text())
+    print(response.json())
+    return response.json()
 
 def start (update, context):
     update.message.reply_text("Nice to meet you! I am just a prototype, but you can start by keying in /ics or /medisave or /medishield to see links to their claimable limits!")
@@ -31,8 +40,11 @@ def medishield(update, context):
     update.message.reply_text(medishield, reply_markup=keyboard)
 
 def ics(update, context):
-    ics = retrieve()['ics']['content']
-    update.message.reply_text(ics)
+    ntuc = retrieve_ics()[0]['org_name']
+    ntuc_details = retrieve_ics()[0]['details']
+    ntuc_contact = retrieve_ics()[0]['contact']
+    ntuc_lastupdate = retrieve_ics()[0]
+    update.message.reply_text(ntuc + ntuc_details + ntuc_contact + ntuc_lastupdate)
 
 def main():
     print("MSW Bot started")
